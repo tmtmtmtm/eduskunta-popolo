fe2a97e60bce6c02a201a418f6800d10f33f13cd
Tony Bowden <tony@tmtm.com>
2014-05-09 08:35:55 +0300

    popit_add_coalitions.rb
    Simple script to add all the coalition information to PopIt

---
+++ bin/popit_add_coalitons.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/ruby 
+
+require 'popit'
+
+password = ARGV[0] or abort "Usage: #{$0} <password>"
+
+api = PopIt.new :instance_name => 'eduskunta', :user => 'tony@micropiphany.com', :password => password
+
+cs = JSON.parse(File.read('coalitions.json'))
+
+cs.first(1).each { |c| 
+  ms = c.delete('memberships')
+  puts "Putting #{c}"
+  api.organizations.post(c); 
+
+  ms.each { |m| 
+    m['organization_id'] = c['id'];
+    puts "  Membership: #{m}"
+    api.memberships.post(m)
+  }
+}
+
+
+
